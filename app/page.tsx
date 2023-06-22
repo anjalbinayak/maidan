8dec1b285cf279fca7c8ad77c319657ea99b78f7
Anjal Binayak <anjalbinayak3@gmail.com>
2023-06-22 17:58:38 +0545

    style basic user profile

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,31 @@ import { signIn, signOut, useSession } from "next-auth/react";
 export default function Home() {
   const { data: session } = useSession();
 
+  const styles = {
+    button: {
+      padding: "1rem",
+      border: "1px solid #ccc",
+      borderRadius: "4px",
+      margin: "1rem",
+      width: "300px",
+      backgroundColor: "#333",
+      color: "white",
+
+      "&:hover": {
+        backgroundColor: "red",
+      },
+    },
+  };
+
   session && console.log(session);
   return (
     <div>
       {session && (
         <div>
           <UserProfile {...session?.user} idx={"23"} />
-          <button onClick={() => signOut()}>Sign out</button>
+          <button style={styles.button} onClick={() => signOut()}>
+            Sign out
+          </button>
         </div>
       )}
 
